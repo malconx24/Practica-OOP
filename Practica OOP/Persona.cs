85d2deaa2e0dfcf2769902c6e7c3acbc53905857
juan manuel javier <lomanito@hotmail.com>
2025-04-29 11:30:59 -0400

    Agregar archivos de proyecto.

---
+++ Practica OOP/Persona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_OOP
+{
+    internal class Persona
+    {
+        // Atributos
+       public string Nombre { get; set; }
+      public int Edad { get; set; }
+        public string Ciudad { get; set; }
+    }
+}
